a1c5fd7980722c83f870292d133d4a70a1941d94
Bresnier moreno <bressmoreno20@gmail.com>
2024-06-07 19:15:25 -0400

    Initial Commit

---
+++ UnlimeTest/UnlimeTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnlimeTestApp.swift
+//  UnlimeTest
+//
+//  Created by Bresnier Moreno on 7/6/24.
+//
+
+import SwiftUI
+
+@main
+struct UnlimeTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
